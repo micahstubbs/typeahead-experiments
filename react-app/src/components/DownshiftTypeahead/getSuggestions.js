61b3fc3a621b5143b0f083081279b954d2763f6e
Micah Stubbs <micah.stubbs@gmail.com>
2019-04-12 23:33:51 -0700

    getSuggestions

---
+++ react-app/src/components/DownshiftTypeahead/getSuggestions.js
@@ -0,0 +1,22 @@
+import deburr from "lodash/deburr";
+import { suggestions } from "./constants";
+
+export default function getSuggestions(value) {
+  const inputValue = deburr(value.trim()).toLowerCase();
+  const inputLength = inputValue.length;
+  let count = 0;
+
+  return inputLength === 0
+    ? []
+    : suggestions.filter(suggestion => {
+        const keep =
+          count < 5 &&
+          suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
+
+        if (keep) {
+          count += 1;
+        }
+
+        return keep;
+      });
+}
--- react-app/src/components/DownshiftTypeahead/index.js
+++ react-app/src/components/DownshiftTypeahead/index.js
@@ -1,35 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import deburr from "lodash/deburr";
 import Downshift from "downshift";
 import { withStyles } from "@material-ui/core/styles";
-
 import Paper from "@material-ui/core/Paper";
 
-import { suggestions } from "./constants";
 import { styles } from "./styles";
 import renderInput from "./renderInput";
 import renderSuggestion from "./renderSuggestion";
-
-function getSuggestions(value) {
-  const inputValue = deburr(value.trim()).toLowerCase();
-  const inputLength = inputValue.length;
-  let count = 0;
-
-  return inputLength === 0
-    ? []
-    : suggestions.filter(suggestion => {
-        const keep =
-          count < 5 &&
-          suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
-
-        if (keep) {
-          count += 1;
-        }
-
-        return keep;
-      });
-}
+import getSuggestions from "./getSuggestions";
 
 function IntegrationDownshift(props) {
   const { classes } = props;
